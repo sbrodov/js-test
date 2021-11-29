da8010d595d0c01a3c7472965edd19e4d553bd64
Alexey Sbrodov <sbrodov.alexey@gmail.com>
2021-11-29 15:38:41 +0500

    js app start

--- js/script.js
+++ js/script.js
@@ -1,6 +1,54 @@
-let hi = 3;
 
-console.log(hi);
+// let a = 1 && 2;
+// let b = 3 || 4;
+
+/* ================================ */
+
+
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        // yourLastMovie: yourLastMovieRate
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+let a = prompt('Один из последних просмотренных фильмов?', '');
+let b = +prompt('На сколько оцените его?', '');
+let c = prompt('Один из последних просмотренных фильмов?', '');
+let d = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+
+console.log(personalMovieDB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
